07cd23b17cdb796b66f85c08a4a419c5cccef350
gorkemerdogan <mgorkem.erdogan@gmail.com>
2024-11-15 10:58:48 +0300

    Updated comments

--- contracts/LotteryStructs.sol
+++ contracts/LotteryStructs.sol
@@ -1,36 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/*
+    This library provides a set of data structures to manage and organize information required for a
+    lottery system. Library has a LotteryInfo struct to manage lottery information and TicketInfo
+    struct for tickets. It can also has an enum to define and manage lottery's state.
+*/
 library LotteryStructs {
 
-    // Enum to represent the different states a lottery can be in
+    /*
+      Enum representing the various states a lottery can go through.
+     */
     enum LotteryState {
-        INACTIVE,   // Lottery has not started
-        ACTIVE,     // Lottery is currently active and tickets can be purchased
-        CLOSED,     // Lottery is closed, no more tickets can be purchased
-        COMPLETED,  // Lottery has ended and winners have been drawn
-        CANCELED    // Lottery has been canceled, refunds may be issued
+        INACTIVE,       // 0: Lottery is not yet started
+        ACTIVE,         // 1: Lottery is ongoing and tickets can be purchased
+        CLOSED,         // 2: Lottery has ended and ticket purchase is not available
+        COMPLETED,      // 3: Lottery is completed and the winner is revealed
+        CANCELED        // 4: Lottery is cancelled
     }
 
-    // Struct to store information about each lottery
+    /*
+        Struct representing a single lottery's information.
+     */
     struct LotteryInfo {
-        uint unixbeg;           // Start time of the lottery in Unix timestamp
-        uint nooftickets;       // Total number of tickets available
-        uint noofwinners;       // Number of winners for this lottery
+        uint unixbeg;           // Start time of the lottery (in Unix timestamp)
+        uint nooftickets;       // Total number of tickets in the lottery
+        uint noofwinners;       // Number of winners to be selected
         uint minpercentage;     // Minimum percentage of tickets to be sold for the lottery to be valid
-        uint ticketprice;       // Price of each ticket in the specified token
-        uint soldTickets;       // Count of tickets sold
-        LotteryState state;     // Current state of the lottery (INACTIVE, ACTIVE, etc.)
-        bytes32 htmlhash;       // Hash of the lottery details page for verification
-        string url;             // URL to the lottery details page
-        address paymentToken;   // ERC20 token used for payment
+        uint ticketprice;       // Price of each ticket (in the NBG Token)
+        uint soldTickets;       // Number of tickets sold
+        LotteryState state;     // Current state of the lottery
+        bytes32 htmlhash;       // Hash of the lottery details page (will be used for off-chain data verification)
+        string url;             // URL of the lottery details page (when needed)
+        address paymentToken;   // Address of the NBG Token contract used for payments
     }
 
-    // Struct to store information about each purchased ticket
+    /*
+        Struct representing details about a purchased ticket or tickets.
+     */
     struct TicketInfo {
-        address participant;        // Address of the ticket buyer
-        uint quantity;              // Quantity of tickets purchased in the transaction
-        bytes32 hash_rnd_number;    // Hash of the random number (used as a commitment for fair revealing)
-        bool revealed;              // Indicates if the random number has been revealed
+        address participant;        // Address of the user who purchased the ticket(s)
+        uint quantity;              // Number of tickets purchased in this transaction
+        bytes32 hash_rnd_number;    // Commitment hash of a random number (used for fair lottery mechanics)
+        bool revealed;              // Indicates if the participant has revealed their random number
     }
 }
