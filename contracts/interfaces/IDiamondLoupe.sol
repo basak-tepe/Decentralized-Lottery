591ee8908e41587dc0a405bc6825969c04f5ec04
Basak Tepe <basaktepe2020@gmail.com>
2025-01-11 21:14:00 +0300

    Revert changes

--- contracts/interfaces/IDiamondLoupe.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IDiamondLoupe {
-    function facetAddress(bytes4 _functionSelector) external view returns (address);
-    function facetAddresses() external view returns (address[] memory);
-}
